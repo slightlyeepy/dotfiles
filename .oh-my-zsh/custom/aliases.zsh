6d4069087145d89e2ac3643a54085c52c1f4693f
me <probablymyemail@proton.me>
2024-11-01 17:03:05 +0500

    add zsh aliases

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,10 @@
+alias c='clear'
+alias dot='git --git-dir=$HOME/.dot/ --work-tree=$HOME'
+alias gls='git ls-files'
+alias hyfetch="hyfetch --args '--package_managers tiny --disable model uptime resolution cpu gpu memory network bluetooth bios' --distro arch_small"
+alias mmp='mpv --no-video --term-osd-bar --no-resume-playback --loop=inf'
+alias qfind='find . -name '
+alias sway-nvidia='sway --unsupported-gpu -D noscanout'
+alias trim='doas fstrim --listed-in /etc/fstab:/proc/self/mountinfo --verbose --quiet-unsupported'
+alias valleakchk='valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes'
+alias zwsp="printf '\u200b'"
